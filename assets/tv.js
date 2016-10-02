a1d7f5045fb24b9649b6c00890cedc4ae8f22f02
HIRANO Takahito <hiranotaka@zng.info>
2016-10-02 23:43:07 +0900

    Support ajax post
    Signed-off-by: HIRANO Takahito <hiranotaka@zng.info>

--- assets/tv.js
+++ assets/tv.js
@@ -2,7 +2,18 @@ function updatePositions() {
     $('td.hour').css('left', $(window).scrollLeft() + 'px');
     $('td.program').css('top', $(window).scrollTop() + 'px');
 }
+
 $(window).scroll(function () {
     updatePositions();
 });
 updatePositions();
+
+$(window).submit(function(event) {
+    var target = $(event.target);
+    $('.main').parent().load(
+	'./?mode=html .main', target.serializeArray(), function() {
+	    updatePositions();
+	});
+    $('.main :input').prop('disabled', true);
+    event.preventDefault();
+});
